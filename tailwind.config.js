7cdc6c19b06fa19afdcc0bfd2bc5645153d27419
syeddaniyalbokhari72 <syeddaniyalbokhari72@gmail.com>
2025-08-13 11:19:23 +0000

    fixed build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'bg-dark-900',
+  ],
   theme: {
     extend: {
       colors: {
@@ -53,4 +56,4 @@ export default {
     },
   },
   plugins: [],
-} 
+}
