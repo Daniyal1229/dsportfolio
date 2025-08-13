d44ec13528a45cca726355ce3f010cabbf77483e
syeddaniyalbokhari72 <syeddaniyalbokhari72@gmail.com>
2025-08-13 11:11:41 +0000

    fixed typescript issue

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -39,7 +39,7 @@ const Projects = () => {
           viewport={{ once: true }}
           className="grid md:grid-cols-2 lg:grid-cols-3 gap-8"
         >
-          {projects.map((project, index) => (
+          {projects.map((project) => (
             <motion.div
               key={project.id}
               variants={itemVariants}
